092a21eebbe823cf34a8982dd97e5b90c9bd71fa
marmo002 <marmo_02@hotmail.com>
2017-10-12 03:26:21 -0400

    Add the h1 inside header a title. Do this on the Pages controller, on each page method using the @header instance variable

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,14 +2,15 @@ class PagesController < ApplicationController
 
   def welcome
     # render :about It will render the view about instead of welcome
+    @header = "This is the Welcome Page"
   end
 
   def about
-
+    @header = "This is the 'This is us' page"
   end
 
   def contest
-
+    @header = "You are in the Contest page now"
   end
 
 end
